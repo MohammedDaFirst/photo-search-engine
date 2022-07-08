f42aea293d61d0c81d329f6a8cfe57626f2d581f
lucian <luciferiizero@gmail.com>
2022-07-08 17:59:32 +0300

    1.0v

--- src/Photo.js
+++ src/Photo.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
-const Photo = () => {
-  return <h2>photo component</h2>
+const Photo = ({ urls: { regular },
+  alt_description,
+  likes,
+  user: { name,
+    portfolio_url,
+    profile_image: { medium }
+  }
+}) => {
+  return <article className="photo">
+    <img src={regular} alt={alt_description} />
+    <div className="photo-info">
+      <h4>{name}</h4>
+      <h4>{likes} likes</h4>
+
+      <a href={portfolio_url} target="_blank" rel="noopener noreferrer" >
+        <img src={medium} alt='' className='user-img' />
+      </a>    </div>
+  </article>
 }
 
 export default Photo
